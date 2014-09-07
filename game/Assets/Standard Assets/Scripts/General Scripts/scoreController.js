1625bfbea5969c0b8922ccde3234e0d679b9b9c6
Brad Murphy <bradmurphy@Brads-MacBook-Pro.local>
2014-09-07 18:25:40 -0400

    Added game clock

--- game/Assets/Standard Assets/Scripts/General Scripts/scoreController.js
+++ game/Assets/Standard Assets/Scripts/General Scripts/scoreController.js
@@ -1,17 +1,30 @@
 ﻿#pragma strict
 
-private var LevelController:LevelController;
-
-
+private var LevelController: LevelController;
+private var startTime: float;
+var textTime: String;
 
 function Start () {
 
-	LevelController = gameObject.Find(
-		"LevelController").GetComponent("LevelController");
+	startTime = Time.time;
+	LevelController = gameObject.Find("LevelController").GetComponent("LevelController");
+
 }
 
 function OnGUI () {
-	guiText.text = LevelController.currentScore.ToString();
+
+	// SUBTRACT START TIME FROM CURRENT TIME
+	var guiTime = Time.time - startTime;
+
+	// MINUTE, SECOND, FRACTION VARIABLES
+	var minutes : int = guiTime / 60; //Divide the guiTime by sixty to get the minutes.
+	var seconds : int = guiTime % 60;//Use the euclidean division for the seconds.
+	var fraction : int = (guiTime * 100) % 100;
+	
+	// SET TIMER TO STRING FORMAT
+	textTime = String.Format ("{0:00}:{1:00}:{2:00}", minutes, seconds, fraction); 
+	// APPEND TIME TO TIMER
+	GetComponent(GUIText).text = textTime;
 
 }
 
--- game/Assets/character/characterController.js
+++ game/Assets/character/characterController.js
@@ -6,10 +6,10 @@
 // GLOBAL VARIABLE
 var walkSpeed: int = 7;
 var jumpHeight: int = 5;
-var spawn:GameObject;
-var spotlight:GameObject;
-var paused: boolean;
-var speedy: boolean;
+var spawn: GameObject;
+var spotlight: GameObject;
+private var paused: boolean;
+private var speedy: boolean;
 
 function OnTriggerEnter (other : Collider) {
 	Debug.Log("You have picked something up");
@@ -17,7 +17,9 @@ function OnTriggerEnter (other : Collider) {
 }
 
 function Start () {
+
 	spotlight = GameObject.Find("Spotlight");
+
 }
 
 function Update () {
@@ -30,12 +32,9 @@ function Update () {
 		if (paused === true) {
 			Time.timeScale = 0;
 			audio.Pause();
-
-
 		} else {
 			Time.timeScale = 1;
 			audio.Play();
-			
 		}
 
 	}
@@ -77,10 +76,12 @@ function Update () {
 }
 
 function DeathByTrigger() {
+
 	Debug.Log("die by trigger enemy");
 
 	transform.position.x = spawn.transform.position.x;
 	transform.position.y = spawn.transform.position.y;
+
 }
 
 function OnCollisionEnter2D (other : Collision2D) {
@@ -92,21 +93,15 @@ function OnCollisionEnter2D (other : Collision2D) {
 	transform.position.x = spawn.transform.position.x;
 	transform.position.y = spawn.transform.position.y;
 	Debug.Log("DIE");
-	//transform.position.x =	-119.0245;
-	//transform.position.y = 	5.513453;
+
 }
 
 function FixedUpdate () {
 
-	//make Character move
 	rigidbody2D.velocity.x = walkSpeed;
 
 	var start = transform.position;
 	start.y -= 1.4;
-
-	//Debug.DrawRay(start, -Vector2.up, Color.red, 10);
-
-	//var ray:RaycastHit2D = Physics2D.Raycast(start, -Vector2.up, 0.01);
 
 	var animationController:Animator = this.GetComponent("Animator");
 
@@ -115,22 +110,6 @@ function FixedUpdate () {
 	var smooth = 2.0;
 	var target = Quaternion.Euler (0, 0, YVel * 2);
 	transform.rotation = Quaternion.Slerp(transform.rotation, target, Time.deltaTime * smooth);
-	
-	// adjust the light's position.y relative to the sub's velocity.y 	
-		// spotlight.transform.position.y = 0;//= YVel * .001;
-		// spotlight.transform.position.x = 0;
-		
-//	var LVel = spotlight.position.y;
-//	var target2 = Quaternion.Euler (0, 0, YVel * 1.5);
-//	transform.rotation = Quaternion.Slerp(transform.rotation, target2, Time.deltaTime * smooth);
-	
-
-	// if (ray.collider) {
-		
-	// 	animationController.SetInteger("state", 0);
-	// }else {
-	// 	animationController.SetInteger("state", 1);
-	// };
 
 	if (Input.GetAxis("Vertical") > 0) {
 			// rigidbody2D.velocity.y = jumpHeight;
@@ -140,17 +119,5 @@ function FixedUpdate () {
 			rigidbody2D.AddRelativeForce(Vector3.up * 40);	
 		}
 	};
-
-	// CHECK AND SEE IF USER IS HITING BUTTON/CONTROLLER
-	// if (Input.GetAxis("Vertical")){
-	// 	rigidbody2D.velocity.y = jumpHeight;
-	// }
-
-	// if (transform.position.y <-3) {
-	// 	Debug.Log("Die Mario!");
-	// 	transform.position.x = spawn.transform.position.x;
-	// 	transform.position.y = spawn.transform.position.y;
-	// };
-
 	
 }
